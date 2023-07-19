12846d89f2e00ed88ed6d990bb55728a6229c4bb
Mark Rucker <rucker.mark@gmail.com>
2023-07-18 20:01:28 -0700

    Updated to latest coba version.

--- corrals.py
+++ corrals.py
@@ -3,7 +3,6 @@ import coba as cb
 import numpy as np
 import scipy.optimize as so
 
-from itertools import count
 from normalizers import BettingNormCS
 
 class CorralSmoothIGW:
@@ -47,10 +46,10 @@ class CorralSmoothIGW:
 
         self.invpalgo = torch.Tensor(invp + root, device=self.invpalgo.device)
 
-    def sample(self, fhatstar, ahatstar, fhat, X):
+    def sample(self, fhat, X, fhatstar, ahatstar):
 
         #This is a "batch" of data.. So we need to pick a base for each of the N items in the batch
-        N = fhatstar.shape[0]
+        N = len(X)
 
         #select a base algorithm N times according to the current corral distribution
         #interestingly, this appears to be considerably faster than using random.choices
@@ -60,13 +59,13 @@ class CorralSmoothIGW:
         tau      = self.taus.gather(dim=0,index=algo).unsqueeze(1)
         self.T += 1 
 
-        self.gamma = self.gscale(self.gzero,self.gamma,self.T)
+        self.gamma = self.gscale(self.gzero,self.T)
 
         #Sampling once from our base measure for each example in the batch.
         #Our base measure in this case is the uniform distrubtion in [0,1].
         arando = torch.rand(size=(N, 1), device=X.device)
         fhatrando = fhat(X, arando)    
-        probs = tau / (tau + self.gamma*(fhatstar-fhatrando))        
+        probs = tau / (tau + self.gamma*(fhatrando-fhatstar))        
         unif  = torch.rand(size=(N, 1), device=X.device)
 
         #shouldexplore is a Bernoulli random variable 
@@ -121,90 +120,80 @@ class CorralCappedIGW:
 
         self.invpalgo = torch.Tensor(invp + root, device=self.invpalgo.device)
 
-    def sample(self, fhatstar, ahatstar, fhat, X):
+    def sample(self, fhat, X, fhatstar, ahatstar):
+
+        if 'samples_martin' not in cb.CobaContext.learning_info:
+            cb.CobaContext.learning_info['samples_martin'] = []
+
+        if 'samples_reject' not in cb.CobaContext.learning_info:
+            cb.CobaContext.learning_info['samples_reject'] = []
 
         #This is a "batch" of data.. So we need to pick a base for each of the N items in the batch
-        N = fhatstar.shape[0]
+        N = len(X)
 
         algos    = torch.distributions.categorical.Categorical(probs=1.0/self.invpalgo, validate_args=False).sample((N,))
         invpalgo = torch.gather(input=self.invpalgo.unsqueeze(0).expand(N, -1), dim=1, index=algos.unsqueeze(1))
-        tau      = torch.gather(input=self.taus.unsqueeze(0).expand(N, -1), dim=1, index=algos.unsqueeze(1))
+        taus     = torch.gather(input=self.taus.unsqueeze(0).expand(N, -1), dim=1, index=algos.unsqueeze(1))
 
         selected_actions  = []
         selected_density  = []
 
         self.T += 1 
 
-        self.gamma = self.gscale(self.gzero,self.gamma,self.T)
-        
-        for i,t,x,A,F in zip(count(),tau,X,ahatstar,fhatstar):
-            t = t.item()
+        self.gamma = self.gscale(self.gzero,self.T)
 
-            infinium = (fhat(x,0) if A > .5 else fhat(x,1)).item()
-            supremum = F.item()
+        for tau,x in zip(taus,X):
 
-            if self.kappa_infty == 1:
-                beta = self.find_beta_montecarlo(fhat,self.gamma,x,t,infinium,supremum,1000,1)
-            else:
-                beta = self.find_beta_martingale(fhat,self.gamma,x,t,supremum)
+            beta = self.find_beta_martingale(fhat,self.gamma,x,tau.item())
+            m_t  = lambda a: 1/(self.lamb+self.gamma*torch.clamp(fhat(x,a)-beta,min=0))
 
-            m_t        = lambda a: t/(self.lamb+self.gamma*torch.clamp((supremum-fhat(x,a))-beta,min=0))
-            normalizer = m_t(A).item()
+            keep = torch.tensor([])
+            n    = 0
 
-            actions   = self.base_action_sampler(5000)
-            densities = m_t(actions).squeeze()
-            keep      = np.random.rand() < densities/normalizer
+            while not keep.any():
+                n        += len(keep)
+                actions   = self.base_action_sampler(1000)
+                densities = m_t(actions).squeeze()
+                keep      = torch.rand(1000) < densities
 
-            if not keep.any():
-                selected_actions.append(A.item())
-                selected_density.append(None)
-            else:
-                index = keep.nonzero()[0].item()
-                selected_actions.append(actions[index].item())
-                selected_density.append(densities[index].item())
+            index = keep.nonzero()[0].item() 
+
+            selected_actions.append(actions[index].item())
+            selected_density.append(densities[index].item())
+            cb.CobaContext.learning_info['samples_reject'].append(n+index)
 
         return selected_actions, selected_density, algos, invpalgo
 
     def base_action_sampler(self, n):
         return torch.rand(size=(n,1))
 
-    def find_beta_montecarlo(self,fhat,gamma,x,tau,inf,sup,n,target):
-        f = sup-fhat(x.repeat(n,1), torch.arange(0,1,1/n).unsqueeze(1)).cpu().numpy()
-        B = lambda beta: target-tau/n*np.sum(1/(self.lamb+gamma*np.clip(f-beta,0,None)))
-        beta, res = so.brentq(B, -tau/gamma, (sup-inf), full_output=True)
-        assert res.converged, f"We failed to find a root: {res}."
-
-        return beta
-
-    def find_beta_martingale(self,fhat,gamma,x,tau,sup):
+    def find_beta_martingale(self,fhat,gamma,x,tau):
 
         clip_min = np.core.umath.maximum
 
+        #Note, in the g below f==(self.lamb+gamma*fhat) and beta==(gamma*beta)
+        #So, it is g = lambda f,beta: tau/max(<self.lamb+gamma*(fhat-beta)>,self.lamb)
+        #we do it this crazy kind of way to reduce the repeated calculations in g
+
         alpha = .05
-        g     = lambda f,beta: tau/clip_min(f-beta,self.lamb)
+        g     = lambda f,beta: tau/clip_min(f-beta,self.lamb) 
         cs    = BettingNormCS(g=g, tau=tau, gamma=gamma, alpha=alpha, lb=1/self.kappa_infty)
 
-        T  = 10_000 # we shouldn't ever hit this but just in case...
-        fs = []
+        N  = 10_000 # we shouldn't ever hit this but just in case...
 
         def batched_base_action_sampler():
             while True:
-                yield from fhat(x,self.base_action_sampler(100)).squeeze().tolist()
+                yield from fhat(x,self.base_action_sampler(1000)).squeeze().tolist()
 
-        for t,f in zip(range(T),batched_base_action_sampler()):
-            
-            fs.append(f)
-            cs.addobs(self.lamb+gamma*(sup-f))
+        for n,f in zip(range(N),batched_base_action_sampler()):
 
-            if t % 10 == 0:
+            cs.addobs(self.lamb+gamma*f)
+
+            if n % 10 == 0:
                 cs.updatelowercs()
                 cs.updateuppercs()
                 l, u = cs.getci()
+                if u-l < .0001: break
 
-            if l > u: break
-
-        if 'samples' not in cb.CobaContext.learning_info:
-            cb.CobaContext.learning_info['samples'] = []
-        cb.CobaContext.learning_info['samples'].append(t)
-        
+        cb.CobaContext.learning_info['samples_martin'].append(n)
         return min(u,l) + torch.rand(size=(1,)).item()*abs(u-l)
